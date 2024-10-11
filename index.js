fd8fea6f07d38ce568ecff921e85b1af6004cc77
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-10-11 15:15:17 +0600

    first video done

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const loadAllPhones = () => {
+const loadAllPhones = async () => {
   console.log("Wow 3 seconds gone");
+  document.getElementById("spinner").style.display = "none";
+
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=iphone`
+  );
+  const data = await response.json();
+  displayAllPhones(data.data);
+};
+
+const displayAllPhones = (phones) => {
+  console.log(phones);
 };
 
 const handleSearch = () => {
+  document.getElementById("spinner").style.display = "block";
+
   setTimeout(function () {
     loadAllPhones();
   }, 2000);
 };
+
+loadAllPhones();
